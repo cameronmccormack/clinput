bdd34609998cbdef5e4a235c99f76cdaabc14cff
Cameron McCormack <cam.mac@live.co.uk>
2019-09-04 22:56:06 +0100

    added tests for 1 0 and blank

--- tests/test_s_natural.py
+++ tests/test_s_natural.py
@@ -1,25 +1,59 @@
-import pytest
-import io
+from io import StringIO
 import sys
-from clinput import single
+from clinput.single import natural
 
 
 class TestNatural:
-    def test_one(self):
-        output = io.StringIO()
-        sys.stdout = output
-        sys.stdin = io.StringIO("1")
-        value = single.natural("Enter: ")
-        sys.stdout = sys.__stdout__
-        assert output.getvalue() == "Enter: "
+    """Unit tests for the natural number function for single user input."""
+    def test_one_false(self):
+        """Test an input of 1 for natural numbers greater than zero."""
+        sys.stdin = StringIO("1")
+        sys.stdout = StringIO()
+        value = natural("Enter: ")
+        assert sys.stdout.getvalue() == "Enter: "
         assert value == 1
 
-    def test_fail_one(self):
-        output = io.StringIO()
-        sys.stdout = output
-        sys.stdin = io.StringIO("a\n1")
-        value = single.natural("Enter: ")
-        sys.stdout = sys.__stdout__
-        print(output.getvalue())
-        assert output.getvalue() == "Enter: Please enter an integer greater than zero.\nEnter: "
+    def test_one_true(self):
+        """Test an input of 1 for natural numbers greater than or equal to
+        zero.
+        """
+        sys.stdin = StringIO("1")
+        sys.stdout = StringIO()
+        value = natural("Enter: ", zero=True)
+        assert sys.stdout.getvalue() == "Enter: "
         assert value == 1
+
+    def test_zero_false(self):
+        """Test an input of 0 for natural numbers greater than zero."""
+        sys.stdin = StringIO("0\n1")  # last input 1 to break input loop
+        sys.stdout = StringIO()
+        value = natural("Enter: ")
+        assert sys.stdout.getvalue() == ("Enter: Please enter an integer "
+                                         "greater than zero.\nEnter: ")
+        assert value == 1
+
+    def test_zero_true(self):
+        """Test an input of 1 for natural numbers greater than or equal to
+        zero.
+        """
+        sys.stdin = StringIO("0\n1")  # last input 1 shouldn't be reached
+        sys.stdout = StringIO()
+        value = natural("Enter: ", zero=True)
+        assert sys.stdout.getvalue() == "Enter: "
+        assert value == 0
+
+    def test_blank(self):
+        """Test a blank input."""
+        sys.stdin = StringIO("\n1")  # last input 1 to break input loop
+        sys.stdout = StringIO()
+        value = natural("Enter: ")
+        assert sys.stdout.getvalue() == ("Enter: Please enter an integer "
+                                         "greater than zero.\nEnter: ")
+        assert value == 1
+
+    def test_none(self):
+        """Test a null input."""
+        sys.stdin = StringIO(None)
+        sys.stdout = StringIO()
+        value = natural("Enter: ")
+        assert value = None
