77f9f8fcc34cd70e9b6018c72e04eabf7b555177
Cameron McCormack <cam.mac@live.co.uk>
2019-09-04 23:18:15 +0100

    added tests for edge cases

--- tests/test_s_natural.py
+++ tests/test_s_natural.py
@@ -47,6 +47,72 @@ class TestNatural:
         sys.stdin = StringIO("\n1")  # last input 1 to break input loop
         sys.stdout = StringIO()
         value = natural("Enter: ")
+        assert sys.stdout.getvalue() == ("Enter: Please provide an input."
+                                         "\nEnter: ")
+        assert value == 1
+
+    def test_big(self):
+        """Test a really big number."""
+        sys.stdin = StringIO("835013851341353317591735386482")
+        sys.stdout = StringIO()
+        value = natural("Enter: ")
+        assert sys.stdout.getvalue() == "Enter: "
+        assert value == 835013851341353317591735386482
+
+    def test_negative(self):
+        """Test a negative integer."""
+        sys.stdin = StringIO("-43\n1")  # last input 1 to break input loop
+        sys.stdout = StringIO()
+        value = natural("Enter: ")
         assert sys.stdout.getvalue() == ("Enter: Please enter an integer "
                                          "greater than zero.\nEnter: ")
+        assert value == 1
+
+    def test_string(self):
+        """Test a non-number string."""
+        sys.stdin = StringIO("spam\n1")  # last input 1 to break input loop
+        sys.stdout = StringIO()
+        value = natural("Enter: ")
+        assert sys.stdout.getvalue() == ("Enter: Please enter an integer "
+                                         "greater than zero.\nEnter: ")
+        assert value == 1
+
+    def test_float(self):
+        """Test a non-integer positive number."""
+        sys.stdin = StringIO("4.6\n1")  # last input 1 to break input loop
+        sys.stdout = StringIO()
+        value = natural("Enter: ")
+        assert sys.stdout.getvalue() == ("Enter: Please enter an integer "
+                                         "greater than zero.\nEnter: ")
+        assert value == 1
+
+    def test_round_float(self):
+        """Test a floating point whole number (this should not be accepted)."""
+        sys.stdin = StringIO("2.0\n1")  # last input 1 to break input loop
+        sys.stdout = StringIO()
+        value = natural("Enter: ")
+        assert sys.stdout.getvalue() == ("Enter: Please enter an integer "
+                                         "greater than zero.\nEnter: ")
+        assert value == 1
+
+    def test_multi_error(self):
+        """Test multiple errors in a row."""
+        sys.stdin = StringIO("spam\nham\neggs\n1")
+        sys.stdout = StringIO()
+        value = natural("Enter: ")
+        assert sys.stdout.getvalue() == ("Enter: Please enter an integer "
+                                         "greater than zero.\n"
+                                         "Enter: Please enter an integer "
+                                         "greater than zero.\n"
+                                         "Enter: Please enter an integer "
+                                         "greater than zero.\n"
+                                         "Enter: ")
+        assert value == 1
+
+    def test_custom_msg(self):
+        """Test custom error message."""
+        sys.stdin = StringIO("spam\n1")  # last input 1 to break input loop
+        sys.stdout = StringIO()
+        value = natural("Enter: ", err="No, not that!")
+        assert sys.stdout.getvalue() == "Enter: No, not that!\nEnter: "
         assert value == 1
