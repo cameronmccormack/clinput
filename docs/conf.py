65ed00c2c061e5cf3351b6f8d9ab328669edbcf1
Cameron McCormack <cam.mac@live.co.uk>
2019-09-11 21:47:31 +0100

    updated files to work with readthedocs.io

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'clinput'
+master_doc = 'index'
 
 # General information about the project.
 project = 'Clinput'
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinxdoc'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -98,15 +98,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
-#
-# This is required for the alabaster theme
-# refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
-}
+# html_sidebars = {}
 
 
 # -- Options for HTMLHelp output ------------------------------------------
