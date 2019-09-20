ecbf5ead2be3561b039b66e3c36190a5fffa679d
Cameron McCormack <cam.mac@live.co.uk>
2019-09-20 15:58:07 +0100

    Updated version number for push to PyPI.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="clinput",
-    version="1.0.1",
+    version="1.0.2",
     description="Useful command line input functions with error checking.",
     long_description=long_description,
     long_description_content_type="text/markdown",
